1d471a8387d84b35f01300416720e58b2806fc1f
cadenmr <caden.m.r@cox.net>
2019-09-30 00:33:34 -0700

    set color vars, add color array, prepare for image production

--- video.py
+++ video.py
@@ -1,4 +1,5 @@
 # This is a class make_video.py and decode_video.py will use
+# Currently using 50 symbols per field
 
 import cv2
 import bitstring
@@ -17,34 +18,49 @@ class Video:
 
     # Automate the encode process
     def encode(self):
+        # Create an empty list for colors
+        color_array = []
+
+        # Iterate through the file path list
         for i in self.files:
+            # Get the raw bit data of the current file and save the original just-in-case
             bit_repr = self._get_bitstring(i)
+            # Chop into 3-bit or less segments and save separately
             bit_repr_chopped = (bit_repr[i:i+3] for i in range(0, len(bit_repr), 3))
+
+            # Iterate through all 3 or less bit segments
             for d in bit_repr_chopped:
+                # Assign color to segment
+                # 3-bit colors
                 if len(d) == 3:
                     if d == '000':
-                        print('black, 000')
+                        color = 'black'
                     elif d == '111':
-                        print('white, 111')
+                        color = 'white'
                     elif d == '100':
-                        print('blue, 100')
+                        color = 'blue'
                     elif d == '110':
-                        print('green, 110')
+                        color = 'green'
                     elif d == '101':
-                        print('red, 101')
+                        color = 'red'
                     elif d == '010':
-                        print('purple, 010')
+                        color = 'purple'
                     elif d == '011':
-                        print('yellow, 011')
+                        color = 'yellow'
                     elif d == '001':
-                        print('pink, 001')
+                        color = 'pink'
                     else:
                         print('ERROR')
+                        break
+                # Sub 3-bit colors
                 else:
                     for item in d:
                         if item == '0':
-                            print('brown,', item)
+                            color = 'brown'
                         elif item == '1':
-                            print('orange,', item)
+                            color = 'orange'
 
-        # print(len([bit_repr[i:i+3000] for i in range(0, len(bit_repr), 3000)]))
+                color_array.append(color)
+
+        # Print out an item from the color array
+        print(list((color_array[i:i+50] for i in range(0, len(color_array), 50)))[0])
