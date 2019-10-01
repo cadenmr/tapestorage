844a470102ba3d6190f0f31d245603515b2a70ae
cadenmr <caden.m.r@cox.net>
2019-10-01 01:28:20 -0700

    use high luma symbol count instead of low chroma symbol count

--- video.py
+++ video.py
@@ -1,5 +1,5 @@
 # This is a class make_video.py and decode_video.py will use
-# Currently using 50 symbols per field
+# Currently using 200 symbols per field
 
 import cv2
 import bitstring
@@ -9,6 +9,7 @@ class Video:
     # Take in a list of files if encoding
     def __init__(self, files=None):
         self.files = files
+        self.__symbol_count = 143354
 
     # Get the bit string representation of a file
     def _get_bitstring(self, file):
@@ -26,41 +27,23 @@ class Video:
             # Get the raw bit data of the current file and save the original just-in-case
             bit_repr = self._get_bitstring(i)
             # Chop into 3-bit or less segments and save separately
-            bit_repr_chopped = (bit_repr[i:i+3] for i in range(0, len(bit_repr), 3))
+            bit_repr_chopped = (bit_repr[i:i+1] for i in range(0, len(bit_repr), 1))
 
             # Iterate through all 3 or less bit segments
             for d in bit_repr_chopped:
                 # Assign color to segment
                 # 3-bit colors
-                if len(d) == 3:
-                    if d == '000':
-                        color = 'black'
-                    elif d == '111':
-                        color = 'white'
-                    elif d == '100':
-                        color = 'blue'
-                    elif d == '110':
-                        color = 'green'
-                    elif d == '101':
-                        color = 'red'
-                    elif d == '010':
-                        color = 'purple'
-                    elif d == '011':
-                        color = 'yellow'
-                    elif d == '001':
-                        color = 'pink'
-                    else:
-                        print('ERROR')
-                        break
-                # Sub 3-bit colors
+                if len(d) == 1:
+                    if d == '0':
+                        color = 'b'
+                    elif d == '1':
+                        color = 'w'
                 else:
-                    for item in d:
-                        if item == '0':
-                            color = 'brown'
-                        elif item == '1':
-                            color = 'orange'
+                    print('ERROR')
+                    break
 
                 color_array.append(color)
 
-        # Print out an item from the color array
-        print(list((color_array[i:i+50] for i in range(0, len(color_array), 50)))[0])
+        # Print the number of fields that it will take to encode the file
+        print(list(((color_array[i:i+self.__symbol_count] for i in range(0, len(color_array), self.__symbol_count))))[0])
+        print(len(list((color_array[i:i+self.__symbol_count] for i in range(0, len(color_array), self.__symbol_count)))))
