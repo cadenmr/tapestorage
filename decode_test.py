0264d8f56974e96df209b785d75e10decd7618a7
cadenmr <caden.m.r@cox.net>
2019-10-10 11:35:25 -0700

    initial stage of decoder merge

--- decode_test.py
+++ decode_test.py
@@ -24,16 +24,11 @@ w_high = np.array([1, 1, 255])
 
 video = cv2.VideoCapture('data.mov')
 
-# image = cv2.imread('QMPlay2_snap_00004.ppm')
-# image = cv2.resize(image, small_res)
-# image_hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-# mask = cv2.inRange(image_hsv, stop_low, stop_high)
-
 # Main crunch
 data = []
+counter = 0
 
 while True:
-    counter = 0
     ret, frame = video.read()
 
     x_pos = data_start_point[0] + 1
@@ -49,41 +44,26 @@ while True:
 
         while True:
             if x_pos == data_end_point[0] + 1:
-                # print(x_pos, y_pos)
                 x_pos = data_start_point[0] + 1
                 y_pos += 1
 
             if mask_stop[y_pos, x_pos]:
-                print('FRAME END')
-                data.append('FE')
-                # print(f'{counter} BITS')
-                # print(x_pos, y_pos)
+                print(counter)
+                counter += 1
                 break
-
-            # symbol = image[y_pos, x_pos, 0]
-            # image[y_pos, x_pos] = [96, 55, 32]
-            #cv2.imshow('window', image)
-            #cv2.waitKey(1)
-            counter += 1
 
             if b_mask[y_pos, x_pos]:
                 data.append(False)
-                #print(False)
             elif w_mask[y_pos, x_pos]:
                 data.append(True)
-                #print(True)
             else:
-                print('FRAME END')
-                # print(x_pos, y_pos)
-                # cv2.imshow('window', image)
-                # cv2.waitKey(0)
                 break
 
             x_pos += 1
     else:
         break
 
-with open('DECODETEST.txt', 'wb') as file:
+with open('DECODETEST.mp4', 'wb') as file:
     file.write(bitstring.BitArray(data).tobytes())
 
 print(len(data))
--- video.py
+++ video.py
@@ -42,18 +42,54 @@ class Video:
                               self.__initial_resolution[1] - self.__side_pad)
 
         # Speed
-        self.__framerate = 29.97
+        self._framerate = 29.97
 
         # Statistics
         self.__symbol_count = self.__data_end_px[0] * self.__data_end_px[1]
+        self.__transfer_speed = (self.__symbol_count * self._framerate) / 1e+6
 
-        self.__transfer_speed = (self.__symbol_count * self.__framerate) / 1e+6
+        # Initialize variables
+        self._x_pos = self.__data_start_px[0]
+        self._y_pos = self.__data_start_px[1]
+
+        # Create the pointer but don't assign anything to save memory
+        self._frame = None
+
+        self._bar_state = False
+
+    def _reset_framevars(self):
+        # Set cursor
+        self._x_pos = self.__data_start_px[0]
+        self._y_pos = self.__data_start_px[1]
+        # Make an empty image
+        self._frame = np.zeros((self.__initial_resolution[1], self.__initial_resolution[0], 3))
+
+        # Set the background color
+        for i in range(self.__initial_resolution[1]):
+            for b in range(self.__initial_resolution[0]):
+                self._frame[i, b] = self.__background_color
+
+        # Create stripes
+        for i in range(self.__ref_line_start[0], self.__ref_line_end[0]):
+            if self._bar_state:
+                if i % 2 == 0:
+                    self._frame[self.__ref_line_start[1], i] = np.array((0, 0, 0))
+                else:
+                    self._frame[self.__ref_line_start[1], i] = np.array((255, 255, 255))
+            else:
+                if i % 2 == 0:
+                    self._frame[self.__ref_line_start[1], i] = np.array((255, 255, 255))
+                else:
+                    self._frame[self.__ref_line_start[1], i] = np.array((0, 0, 0))
+
+        # Flip the state bit
+        self._bar_state = not self._bar_state
 
     # Automate the encode process
     def encode(self):
         # Show some data
         print(f'Format: {self.__format}')
-        print(f'Framerate: {self.__framerate}')
+        print(f'Framerate: {self._framerate}')
         print(f'Bits per frame: {self.__symbol_count}')
         print(f'Transfer Speed (mbps): {self.__transfer_speed}')
         print('-------------------------------------- ')
@@ -69,97 +105,59 @@ class Video:
 
         # Chop raw bits into frames
         print('chopping bits into frames...')
-        parsed_array = [bs[i:i+self.__symbol_count] for i in range(0, len(bs), self.__symbol_count)]
+        # parsed_array = [bs[i:i+self.__symbol_count] for i in range(0, len(bs), self.__symbol_count)]
         bslen = len(bs)
-        del bs
 
-        time = round((len(parsed_array) / self.__framerate) / 60, ndigits=1)
+        # time = round((len(parsed_array) / self._framerate) / 60, ndigits=1)
+        time = 'DISABLED'
 
         print('done')
         print('-----------------------------')
         print(f'{bslen} bits')
-        print(f'need to make {len(parsed_array)} frames')
+        print(f'need to make {len("abc")} frames')
         print(f'video will be {time} mins')
         print('-----------------------------')
 
         # Begin data encoding
         # Set initial values for the x and y position of the pixel, and initial value for the file name
         filename = 0
-        state = False
 
-        # Run though all the frame data
-        print('making frames...')
-        for d in parsed_array:
-            # Start of frame
-            # Set starter values
-            x_pos = self.__data_start_px[0]
-            y_pos = self.__data_start_px[1]
+        print('making frames (v2)...')
+        # Start by resetting the framing variables and generating a new frame
+        self._reset_framevars()
 
-            # Create blank image
-            frame = np.zeros((self.__initial_resolution[1], self.__initial_resolution[0], 3))
+        # For every item (True/False) in the bitstring object
+        for b in bs:
+            # Line length limit
+            if self._x_pos >= self.__data_end_px[0]:
+                self._x_pos = self.__data_start_px[0]
+                self._y_pos += 1
 
-            # Run through pixels and set background color for each
-            for i in range(self.__initial_resolution[1]):
-                for b in range(self.__initial_resolution[0]):
-                    frame[i, b] = self.__background_color
+            # End of frame limit
+            if self._y_pos > self.__data_end_px[1]:
+                # Resize the frame
+                self._frame = cv2.resize(self._frame, self.__final_resolution, interpolation=cv2.INTER_NEAREST)
+                # Save it
+                cv2.imwrite(f'outputfiles/output{filename}.png', self._frame)
+                # Increment the file name
+                filename += 1
+                # Reset everything and generate a new frame
+                self._reset_framevars()
 
-            # Create the stripes for synchro
-            for i in range(self.__ref_line_start[0], self.__ref_line_end[0]):
+            # Set bit
+            if b:
+                self._frame[self._y_pos, self._x_pos] = np.array((255, 255, 255))
+            elif not b:
+                self._frame[self._y_pos, self._x_pos] = np.array((0, 0, 0))
 
-                if state:
-                    if i % 2 == 0:
-                        frame[self.__ref_line_start[1], i] = np.array((0, 0, 0))
-                    else:
-                        frame[self.__ref_line_start[1], i] = np.array((255, 255, 255))
-                else:
-                    if i % 2 == 0:
-                        frame[self.__ref_line_start[1], i] = np.array((255, 255, 255))
-                    else:
-                        frame[self.__ref_line_start[1], i] = np.array((0, 0, 0))
-
-            # Flip the state for the synchro bar
-            state = not state
-
-            # Check each bit in the frame and set the pixel accordingly
-            for i in d:
-                # Position limit checks
-                # Column limiter
-                if x_pos >= self.__data_end_px[0]:
-                    x_pos = self.__data_start_px[0]
-                    y_pos += 1
-
-                # Line limiter
-                if y_pos >= self.__data_end_px[1]:
-                    y_pos = self.__data_start_px[1]
-
-
-                # 1 bit
-                if i:
-                    frame[y_pos, x_pos] = np.array((255, 255,  255))
-                # 0 bit
-                else:
-                    frame[y_pos, x_pos] = np.array((0, 0, 0))
-
-                # Update the x position every bit
-                x_pos += 1
-
-            # Resize the frame to final resolution
-            frame = cv2.resize(frame, self.__final_resolution, interpolation=cv2.INTER_NEAREST)
-            # Save it & get rid of it
-            cv2.imwrite(f'outputfiles/output{filename}.png', frame)
-            del frame
-            # Increment the file name
-            filename += 1
+            self._x_pos += 1
 
         print('done')
-
-        # Get rid of the bit lists to save memory
-        del parsed_array
 
         # Create a video
         print('converting frames to video')
         # LOSSLESS: hfyu ,
-        out = cv2.VideoWriter('data.mp4', cv2.VideoWriter_fourcc(*'mp4v'), self.__framerate, self.__final_resolution)
+        out = cv2.VideoWriter('data.mov', cv2.VideoWriter_fourcc(*'hfyu'), self._framerate, self.__final_resolution)
         image_list = []
         image_path_list = []
         for name in sorted(glob.glob('outputfiles/output*.png'), key=os.path.getctime):
@@ -178,56 +176,57 @@ class Video:
 
     # Image decoder function
     def decode(self):
-        # Low and high color break bounds
         stop_low = np.array([0, 93, 73])
         stop_high = np.array([180, 255, 151])
 
-        # Open the video source
-        video = cv2.VideoCapture(self.files)
-        # Grab the frame count of video source
-        frame_count = video.get(cv2.CAP_PROP_FRAME_COUNT)
-        data = []
+        b_low = np.array([0, 0, 0])
+        b_high = np.array([1, 1, 1])
 
-        for fr in range(int(frame_count)):
-            x_pos = self.__data_start_px[0]
+        w_low = np.array([0, 0, 254])
+        w_high = np.array([1, 1, 255])
+
+        video = cv2.VideoCapture(self.files)
+
+        # Main crunch
+        data = []
+        counter = 0
+
+        while True:
+            ret, frame = video.read()
+
+            x_pos = self.__data_start_px[0] + 1
             y_pos = self.__data_start_px[1]
 
-            ret, frame = video.read()
-            # frame = cv2.resize(frame, self.__initial_resolution, interpolation=cv2.INTER_NEAREST)
-            frame_hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-
-            mask = cv2.inRange(frame_hsv, stop_low, stop_high)
-
-            # cv2.imshow('Video', frame)
-            # cv2.imshow('Mask', mask)
-
             if ret:
+                image = cv2.resize(frame, self.__initial_resolution, interpolation=cv2.INTER_NEAREST)
+                image_hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+                b_mask = cv2.inRange(image_hsv, b_low, b_high)
+                w_mask = cv2.inRange(image_hsv, w_low, w_high)
+                mask_stop = cv2.inRange(image_hsv, stop_low, stop_high)
+
                 while True:
-                    if x_pos >= self.__data_end_px[0]:
-                        x_pos = self.__data_start_px[0]
+                    if x_pos >= self.__data_end_px[0] + 1:
+                        x_pos = self.__data_start_px[0] + 1
                         y_pos += 1
 
-                    if mask[y_pos, x_pos] == 255:
-                        print('BREAK')
-                        print(x_pos, y_pos)
+                    if mask_stop[y_pos, x_pos]:
+                        # print(counter)
+                        counter += 1
                         break
 
-                    symbol = frame[y_pos, x_pos, 0]
-                    # print(symbol)
-
-                    if int(symbol) < (255/2):
+                    if b_mask[y_pos, x_pos]:
                         data.append(False)
-                        # print(False)
-                    elif (255 / 2) < int(symbol) <= 255:
+                    elif w_mask[y_pos, x_pos]:
                         data.append(True)
-                        # print(True)
                     else:
-                        raise ValueError('Range not within 255')
+                        break
 
                     x_pos += 1
+            else:
+                break
 
-        print(len(data))
+        with open(self.file_out, 'wb') as file:
+            file.write(bitstring.BitArray(data).tobytes())
 
-
-        with open(self.file_out, 'wb') as f:
-            f.write(bitstring.BitArray(data).tobytes())
+        print('Complete')
